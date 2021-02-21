f1a5647fe2d9cb4cbe50e14ae07e3761220ee822
= <mstorey@gmail.com>
2021-02-21 10:31:28 -0500

    first change

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Near Me App - Find things fast!/i);
   expect(linkElement).toBeInTheDocument();
 });
